0d2d80ec8f64155a7fab81f34466f03eb6602cfd
Lucas <lucasferminnieto@yahoo.com.ar>
2024-06-08 21:11:47 -0300

    Tabla de Clientes y Vehiculos

--- interface.py
+++ interface.py
@@ -8,16 +8,16 @@ from database import Database
 class InterfazConcesionario:
     def __init__(self):
         self.vehiculosDb = Database('data/vehiculos.json')
-        self.customDb = Database('data/customers.json')
+        self.customDb = Database('data/clientes.json')
         self.transaccionesDb = Database('data/transacciones.json')
 
     def mainMenu(self):
         while True:
-            print("\n1. Gestionar Vehículos")
+            print("\n1. Gestionar Vehiculos")
             print("2. Gestionar Clientes")
-            print("3. Registrar Transacción")
+            print("3. Registrar Transaccion")
             print("4. Salir")
-            choice = input("Seleccione una opción: ")
+            choice = input("Seleccione una opcion: ")
             if choice == '1':
                 self.modificarVehiculos()
             elif choice == '2':
@@ -27,16 +27,16 @@ class InterfazConcesionario:
             elif choice == '4':
                 sys.exit()
             else:
-                print("Opción invalida, por favor inténtelo nuevamente.")
+                print("Opcion invalida, por favor intentelo nuevamente.")
 
     def modificarVehiculos(self):
         while True:
             print("\n1. Crear Vehiculo")
             print("2. Editar Vehiculo")
             print("3. Eliminar Vehiculo")
-            print("4. Listar Vehículos")
+            print("4. Listar Vehiculos")
             print("5. Volver al menu principal")
-            choice = input("Seleccione una opción: ")
+            choice = input("Seleccione una opcion: ")
             if choice == '1':
                 self.crearVehiculo()
             elif choice == '2':
@@ -48,19 +48,19 @@ class InterfazConcesionario:
             elif choice == '5':
                 return
             else:
-                print("Opción invalida, por favor intente nuevamente.")
+                print("Opcion invalida, por favor intente nuevamente.")
 
     def crearVehiculo(self):
-        # Solicitar datos y crear el vehículo
-        patente = input("Ingrese la patente del vehículo: ")
-        marca = input("Ingrese la marca del vehículo: ")
-        modelo = input("Ingrese el modelo del vehículo: ")
-        tipoVehiculo = input("Ingrese el tipo del vehículo (Sedán, SUV, Pick Up, etc): ")
-        anio = int(input("Ingrese el año del vehículo: "))
-        kilometraje = int(input("Ingrese el kilometraje del vehículo: "))
-        precioCompra = float(input("Ingrese el precio de compra del vehículo: "))
-        precioVenta = float(input("Ingrese el precio de venta del vehículo: "))
-        estado = input("Ingrese el estado del vehículo (Disponible, Reservado, Vendido): ")
+        # Solicitar datos y crear el vehiculo
+        patente = input("Ingrese la patente del vehiculo: ")
+        marca = input("Ingrese la marca del vehiculo: ")
+        modelo = input("Ingrese el modelo del vehiculo: ")
+        tipoVehiculo = input("Ingrese el tipo del vehiculo (Sedán, SUV, Pick Up, etc): ")
+        anio = int(input("Ingrese el año del vehiculo: "))
+        kilometraje = int(input("Ingrese el kilometraje del vehiculo: "))
+        precioCompra = float(input("Ingrese el precio de compra del vehiculo: "))
+        precioVenta = float(input("Ingrese el precio de venta del vehiculo: "))
+        estado = input("Ingrese el estado del vehiculo (Disponible, Reservado, Vendido): ")
 
         vehiculoId = len(self.vehiculosDb.obtenerTodosLosRegistros()) + 1
         nuevoVehiculo = Vehicle(vehiculoId, patente, marca, modelo, tipoVehiculo, anio, kilometraje, precioCompra, precioVenta, estado)
@@ -69,7 +69,7 @@ class InterfazConcesionario:
 
     def editarVehiculo(self):
         # Solicitar ID del vehículo y editar los datos
-        vehiculoId = int(input("Ingrese el ID del vehículo a editar: "))
+        vehiculoId = int(input("Ingrese el ID del vehiculo a editar: "))
         vehiculo = self.vehiculosDb.buscarRegistrosPorId(vehiculoId)
         if vehiculo:
             print("Deje en blanco si no desea modificar el campo.")
@@ -98,8 +98,50 @@ class InterfazConcesionario:
     def listarVehiculos(self):
         # Mostrar todos los vehiculos
         vehiculos = self.vehiculosDb.obtenerTodosLosRegistros()
-        for vehiculo in vehiculos:
-            print(vehiculo)
+        if vehiculos:
+            # Imprimir encabezados de la tabla
+            print("{:<5} {:<10} {:<10} {:<10} {:<15} {:<5} {:<12} {:<15} {:<15}".format(
+                "ID", "Patente", "Marca", "Modelo", "Tipo", "Año", "Kilometraje", "Precio Compra", "Precio Venta"
+            ))
+            print("=" * 100)
+            #Imprimir cada vehiculo en formato de tabla
+            for vehiculo in vehiculos:
+                print("{:<5} {:<10} {:<10} {:<10} {:<15} {:<5} {:<12} {:<15} {:<15}".format(
+                    vehiculo.get('id', 'N/A'),
+                    vehiculo.get('placa', 'N/A'),
+                    vehiculo.get('marca', 'N/A'),
+                    vehiculo.get('modelo', 'N/A'),
+                    vehiculo.get('tipoVehiculo', 'N/A'),
+                    vehiculo.get('anio', 'N/A'),
+                    vehiculo.get('kilometraje', 'N/A'),
+                    vehiculo.get('precioCompra', 'N/A'),
+                    vehiculo.get('precioVenta', 'N/A')
+                ))
+        else:
+            print("No hay vehiculos registrados.")
+
+        
+    def listarClientes(self):
+        # Mostrar todos los clientes en formato de tabla
+        clientes = self.customDb.obtenerTodosLosRegistros()
+        if clientes:
+            # Imprimir encabezados de la tabla
+            print("{:<10} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(
+            "ID", "Nombre", "Documento", "Apellido", "Direccion", "Celular", "Email"))
+            print("=" * 100)
+            # Imprimir cada cliente en formato de tabla
+            for cliente in clientes:
+                print("{:<10} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(
+                    cliente.get('id', 'N/A'),
+                    cliente.get('nombre', 'N/A'),
+                    cliente.get('documento', 'N/A'),
+                    cliente.get('apellido', 'N/A'),
+                    cliente.get('direccion', 'N/A'),
+                    cliente.get('telefono', 'N/A'),
+                    cliente.get('email', 'N/A')
+                ))
+        else:
+            print("No hay clientes registrados.")
 
     def administrarCustomers(self):
         # similar a modificarVehiculos
@@ -109,7 +151,7 @@ class InterfazConcesionario:
             print("3. Eliminar Cliente")
             print("4. Listar Clientes")
             print("5. Volver al Menu Principal")
-            choice = input("Seleccione una opción: ")
+            choice = input("Seleccione una opcion: ")
             if choice == '1':
                 self.crearCustomer()
             elif choice == '2':
@@ -117,19 +159,19 @@ class InterfazConcesionario:
             elif choice == '3':
                 self.eliminarCustomer()
             elif choice == '4':
-                self.listaCustomers()
+                self.listarClientes()
             elif choice == '5':
                 return
             else:
-                print("Opción invalida, por favor intente nuevamente.")
+                print("Opcion invalida, por favor intente nuevamente.")
 
     def crearCustomer(self):
         nombre = input("Ingrese el nombre del cliente: ")
         documento = input("Ingrese el documento del cliente: ")
         apellido = input("Ingrese el apellido del cliente: ")
-        direccion = input("Ingrese la dirección del cliente: ")
-        celular = input("Ingrese el teléfono del cliente: ")
-        email = input("Ingrese el correo electrónico del cliente: ")
+        direccion = input("Ingrese la direccion del cliente: ")
+        celular = input("Ingrese el telefono del cliente: ")
+        email = input("Ingrese el correo electronico del cliente: ")
 
         customerId = len(self.customDb.obtenerTodosLosRegistros()) + 1
         nuevoCustomer = Customer(customerId, nombre, documento, apellido, direccion, celular, email)
@@ -144,9 +186,9 @@ class InterfazConcesionario:
             nombre = input(f"Nombre actual ({customer['nombre']}): ") or customer['nombre']
             documento = input(f"Documento actual ({customer['documento']}): ") or customer['documento']
             apellido = input(f"Apellido actual ({customer['apellido']}): ") or customer['apellido']
-            direccion = input(f"Dirección actual ({customer['direccion']}): ") or customer['direccion']
-            celular = input(f"Teléfono actual ({customer['celular']}): ") or customer['celular']
-            email = input(f"Correo electrónico actual ({customer['email']}): ") or customer['email']
+            direccion = input(f"Direccion actual ({customer['direccion']}): ") or customer['direccion']
+            celular = input(f"Telefono actual ({customer['celular']}): ") or customer['celular']
+            email = input(f"Correo electronico actual ({customer['email']}): ") or customer['email']
 
             actualizarCustomer = Customer(customerId, nombre, documento, apellido, direccion, celular, email)
             self.customDb.actualizarRegistro(customerId, actualizarCustomer.a_dict())
@@ -166,10 +208,10 @@ class InterfazConcesionario:
 
     def administrarTransacciones(self):
         while True:
-            print("\n1. Crear Transacción")
+            print("\n1. Crear Transaccion")
             print("2. Listar Transacciones")
             print("3. Volver al Menu Principal")
-            choice = input("Seleccione una opción: ")
+            choice = input("Seleccione una opcion: ")
             if choice == '1':
                 self.crearTransaccion()
             elif choice == '2':
@@ -177,7 +219,7 @@ class InterfazConcesionario:
             elif choice == '3':
                 return
             else:
-                print("Opción invalida, por favor intente nuevamente.")
+                print("Opcion invalida, por favor intente nuevamente.")
 
     def crearTransaccion(self):
         customerId = int(input("Ingrese el ID del cliente: "))
@@ -187,7 +229,7 @@ class InterfazConcesionario:
         transaccionId = len(self.transaccionesDb.obtenerTodosLosRegistros()) + 1
         nuevoTransaccion = Transaction(transaccionId, customerId, vehiculoId, precioVenta)
         self.transaccionesDb.agregarRegistro(nuevoTransaccion.a_dict())
-        print("Transacción registrada exitosamente.")
+        print("Transaccion registrada exitosamente.")
 
     def listarTransacciones(self):
         transacciones = self.transaccionesDb.obtenerTodosLosRegistros()
