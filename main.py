b2c28a4b86b93f2883ad755fb155478721aff521
e-conte <160084751+e-conte@users.noreply.github.com>
2024-06-13 00:24:43 -0300

    Agregue Comentarios

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from interface import InterfazConcesionario
-
+#   Importa la clase InterfazConcesionario desde el modulo Interface
 def main():
     interface = InterfazConcesionario()
     interface.mainMenu()
-
+    #   llama al método mainMenu de la interface que trajimos dede el import
 
 if __name__ == "__main__":
     main()
+#   Este modulo evita que el módulo interface se ejecute si es importado como parte de otro programa
